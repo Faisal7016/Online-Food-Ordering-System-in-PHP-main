3ac75a58af67957992e4d0a03df9ea91cfb0cf00
Faisal7016 <modanfaisal7016@gmail.com>
2024-08-07 12:26:21 +0530

    Online Food Ordering System

---
+++ Online-Food-Ordering-System-in-PHP-main/admin/include/footer.php
@@ -0,0 +1,2 @@
+ <footer class="footer" style="text-align: center;">CraveCart© 2024 - Online Food Ordering System Developer - <a>CraveCart</a> </footer>
+ 
---
+++ Online-Food-Ordering-System-in-PHP-main/admin/logout.php
@@ -0,0 +1,13 @@
+                <!-- /*!
+* Author Name: CraveCart
+*/ -->
+                <?php
+session_start();
+session_destroy();
+$url = 'index.php';
+header('Location: ' . $url);
+
+?>
+                <!-- /*!
+* Author Name: CraveCart
+*/ -->
